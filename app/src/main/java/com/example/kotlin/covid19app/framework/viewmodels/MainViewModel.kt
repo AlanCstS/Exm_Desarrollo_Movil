2664c1baa3864e695fd46212481e8a33129f1072
unknown <A01708668@tec.mx>
2023-11-26 22:31:13 -0600

    Terminar feature

--- app/src/main/java/com/example/kotlin/covid19app/framework/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/example/kotlin/covid19app/framework/viewmodels/MainViewModel.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.kotlin.covid19app.data.network.model.Country
 import com.example.kotlin.covid19app.data.network.model.CountryObject
 import com.example.kotlin.covid19app.domain.CountryListRequirement
 import com.example.kotlin.covid19app.utils.Constants
@@ -13,12 +14,16 @@ import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
 
-    val countryObjectLiveData = MutableLiveData<CountryObject>()
+    val countryObjectLiveData = MutableLiveData<ArrayList<Country>>()
     private val countryListRequirement = CountryListRequirement()
+    private var result: CountryObject? = null
+
+    private val _searchResults = MutableLiveData<List<Country>>()
+    //val searchResults: LiveData<List<Country>> get() = _searchResults
 
     fun getCountryList() {
         viewModelScope.launch(Dispatchers.IO) {
-            val result: CountryObject? =
+            result =
                 countryListRequirement(Constants.API_DATE, Constants.API_KEY)
             Log.d("Salida", result?.count().toString())
             CoroutineScope(Dispatchers.Main).launch {
@@ -26,4 +31,12 @@ class MainViewModel : ViewModel() {
             }
         }
     }
+
+    fun updateCountryList(filter: String) {
+        if (filter.isNotEmpty()) {
+            val filteredCountries = (result?.filter { country -> country.country.lowercase().contains(filter.lowercase()) }) as ArrayList<Country>
+            countryObjectLiveData.postValue(filteredCountries)
+        }
+    }
+
 }
